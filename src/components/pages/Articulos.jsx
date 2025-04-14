7948b556f00ed9ea24bcc3d10e76dfee580930e1
Emanuel Arias <Emanuelarias9@gmail.com>
2025-04-14 17:36:27 -0500

    Estructura y componentes

---
+++ src/components/pages/Articulos.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Articulos = () => {
+  return <div>Articulos</div>;
+};
---
+++ src/components/pages/Editar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Editar = () => {
+  return <div>Editar</div>;
+};
