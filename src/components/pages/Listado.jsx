b6d9b5065570f167ef2088a7cb2c619d713385dd
Emanuel Arias <Emanuelarias9@gmail.com>
2025-04-21 15:55:55 -0500

    Update Listado component to conditionally render article images based on availability

--- src/components/pages/Listado.jsx
+++ src/components/pages/Listado.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Global } from "../../helpers/Global";
 
 export const Listado = ({ articulos, setArticulos }) => {
   return articulos.map((articulo) => {
     return (
       <article key={articulo._id} className="articulo-item">
         <div className="mask">
-          <img
-            src="https://cdn-images-1.medium.com/max/868/1*yq7TPrTheULIcxwfTD96SA.png"
-            alt="Blog de React"
-          />
+          {articulo.imagen != "default.png" ? (
+            <img
+              src={Global.urlApiBase + "/articulos/imagen/" + articulo.imagen}
+              alt={articulo.titulo}
+            />
+          ) : (
+            <img
+              src="https://cdn-images-1.medium.com/max/868/1*yq7TPrTheULIcxwfTD96SA.png"
+              alt={articulo.titulo}
+            />
+          )}
         </div>
         <div className="datos">
           <h3 className="title">{articulo.titulo} </h3>
