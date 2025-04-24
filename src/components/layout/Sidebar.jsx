9bf893e07057e50aad3d8e892528170005500acf
Emanuel Arias <Emanuelarias9@gmail.com>
2025-04-23 19:11:08 -0500

    Implement search functionality in Sidebar and integrate component for article search results

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Sidebar = () => {
+  const [busquedaState, setbusquedaState] = useState("");
+  const [error, setError] = useState(false);
+  const errorMessage = useRef("");
+  const goTo = useNavigate();
+  const buscar = (e) => {
+    e.preventDefault();
+    let busqueda = e.target.searchBar.value;
+    if (busqueda.trim() === "") {
+      setError(true);
+      setTimeout(() => setError(false), 6000);
+      errorMessage.current = "El campo de busqueda no puede estar vacio";
+    } else {
+      goTo(`/buscador/${busqueda}`);
+    }
+  };
   return (
     <aside className="lateral">
       <div className="search">
         <h3 className="title">Buscador</h3>
-        <form>
-          <input type="text" id="search_field" />
-          <button id="search">Buscar</button>
+        <form onSubmit={buscar}>
+          <input type="text" name="searchBar" />
+          {error && (
+            <div className="toast-notification error">
+              <p>{errorMessage.current}</p>
+            </div>
+          )}
+          <input type="submit" id="search" value="Buscar" />
         </form>
       </div>
     </aside>
--- src/components/pages/Busqueda.jsx
+++ src/components/pages/Busqueda.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Global } from "../../helpers/Global";
+import { apiClientService } from "../../helpers/apiClientService";
+import { Listado } from "./Listado";
+import { useParams } from "react-router-dom";
 
 export const Busqueda = () => {
-  return <div>Busqueda</div>;
+  const [articulos, setArticulos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  const errorMessage = useRef("");
+  useEffect(() => {
+    obtenerArticulos();
+  }, [params]);
+
+  const obtenerArticulos = async () => {
+    const urlPeticion =
+      Global.urlApiBase +
+      "/articulos?titulo=" +
+      params.busqueda +
+      "&contenido=" +
+      params.busqueda;
+    let { apiResponse, loading } = await apiClientService(urlPeticion, "GET");
+    if (apiResponse.status === "Success") {
+      setArticulos(apiResponse.articulos);
+    } else {
+      setArticulos([]);
+      errorMessage.current = apiResponse.mensaje;
+    }
+    setLoading(loading);
+  };
+  return (
+    <>
+      {loading ? (
+        <h1>Cargando...</h1>
+      ) : articulos.length >= 1 ? (
+        <Listado articulos={articulos} setArticulos={setArticulos} />
+      ) : (
+        <h1>{errorMessage.current}</h1>
+      )}
+    </>
+  );
 };
